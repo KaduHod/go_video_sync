fe670b2876a7be47bee9ed06564aad880f19d197
kaduhod <carlosjr.ribas@gmail.com.br>
2025-02-20 12:47:55 -0300

    :art: Inicio dos estilos

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+    module.exports = {
+        content: [
+            "./views/**/*.{html,tmpl,js}",
+            "./public/scripts/**/*.js"
+
+        ],
+        theme: {
+            extend: {},
+        },
+        plugins: [],
+    }
+
