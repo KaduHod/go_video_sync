b1ba337f042311223021c35db91e40dfaa23065c
kaduhod <carlosjr.ribas@gmail.com.br>
2025-02-22 19:56:57 -0300

    :construction: Adicionado middleware para controlar usuarios acessando paginas que nao sao feitas para quem nao tem sessao

---
+++ app/middlewares/onlySessionNotInitializedMiddlware.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"regexp"
+
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+)
+
+func OnlySessionNotInitialized(next echo.HandlerFunc) echo.HandlerFunc {
+    return func(c echo.Context) error {
+        session, err := session.Get("session", c)
+        if err == nil || session == nil || session.Values["user_id"] == nil {
+            room, linkDeConvidado := extractRoomName(c.Request().RequestURI)
+            if linkDeConvidado && c.Request().Method == "GET" {
+                return c.Redirect(303, "/app/join/room/"+room)
+            }
+            return next(c)
+        }
+        return next(c)
+    }
+}
+func extractRoomName(path string) (string, bool) {
+	re := regexp.MustCompile(`^/join/room/([^/]+)$`)
+	matches := re.FindStringSubmatch(path)
+
+	if len(matches) > 1 {
+		return matches[1], true
+	}
+	return "", false
+}
