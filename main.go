e5ec04c39ea00d16688049e057fb2802607423e0
kaduhod <carlosjr.ribas@gmail.com.br>
2025-02-23 23:22:40 -0300

    :bug: Corrigido bug de erro padrao do sistema; que quebra o site e apaga sessao

--- main.go
+++ main.go
@@ -38,18 +38,18 @@ func customHTTPErrorHandler(err error, c echo.Context) {
 	if he, ok := err.(*echo.HTTPError); ok {
 		code = he.Code
 	}
-	//c.Logger().Error(err)
-	errorPage := fmt.Sprintf("%d.html", code)
+	errorPage := "main.tmpl"// fmt.Sprintf("%d.html", code)
 	if err := c.File(errorPage); err != nil {
-		//c.Logger().Error(err)
+		c.Logger().Error(err)
 	}
-    if code == 404 {
-        c.Redirect(303, "/")
+    errors := map[string]string{
+        "user_error": err.Error(),
     }
+    c.Render(code, errorPage, errors)
 }
 func main() {
     e := echo.New()
-    //e.HTTPErrorHandler = customHTTPErrorHandler
+    e.HTTPErrorHandler = customHTTPErrorHandler
     t := &Template{
         templates: template.Must(template.ParseGlob("./views/*.tmpl")),
     }
